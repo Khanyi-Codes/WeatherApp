d278301642c6a04c28aba94f201609f3ee21b08a
Khanyi-Codes <makhubukhanyisile1@gmail.com>
2026-04-13 15:10:55 +0200

    feat: adding search element

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function handleSearchSubmit(event){
     event.preventDefault();
-    
+        let searchInput = document.querySelector(".search-form-input");
+        let cityElement = document.querySelector("#city");
+        cityElement.innerHTML = searchInput.value;
 
 }
 let searchformElement = document.querySelector(".search-form");
