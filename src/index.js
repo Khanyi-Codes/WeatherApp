646b755cd2afa900193235d262f31b8bbdc22dbf
Khanyi-Codes <makhubukhanyisile1@gmail.com>
2026-04-17 17:19:44 +0200

    Feat: Adding real API content to Display forecast function

--- src/index.js
+++ src/index.js
@@ -51,31 +51,41 @@ function getForecast(city){
     axios.get(apiUrl).then(displayForecast);
 
 }
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    
+    return days[date.getDay()];
+ }
 
 
 // Forcast template
-function displayForecast(){
+function displayForecast(response){
+    console.log(response.data);
 
-    let forecastElement = document.querySelector("#forecast");
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+
     let forecastHtml = ``;
-    days.forEach(function(day){
+    response.data.daily.forEach(function(day, index){
+        if(index < 5){
 
     forecastHtml += `<div class = "weather-forecast-day">
-                <div class = "weather-forecast-date">${day}</div>
-                <div class = "weather-forecast-icon">🌧️</div>  
+                <div class = "weather-forecast-date">${formatDay(day.time)}</div>
+                <div class = "weather-forecast-icon"><img src =${day.condition.icon_url} /></div>  
                 <div class = "weather-forecast-temperatures">
                     <div class = "weather-forecast-temperature">
-                    <strong>15°</strong></div> 
-                    <div class= "weather-forecast-temperature">9° </div>
+                    <strong>${Math.round(day.temperature.maximum)}°</strong></div> 
+                    <div class= "weather-forecast-temperature">${Math.round(day.temperature.minimum)}° </div>
                     </div>
-            </div>`;});
-
+            </div>`;
+        }
+        });
+    
+            let forecastElement = document.querySelector("#forecast");
             forecastElement.innerHTML = forecastHtml;
-            
+        
+    
+
 }
-
-
 
 let searchformElement = document.querySelector(".search-form");
 searchformElement.addEventListener("submit", handleSearchSubmit);
